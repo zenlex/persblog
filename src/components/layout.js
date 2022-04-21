84c5ecd78e948285be467349e63fa7ea8d9ea3eb
zenlex <erichrkeil@gmail.com>
2022-04-21 09:59:37 -0500

    fix dark mode toggle lifecycle

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,7 @@ import { Link } from "gatsby"
 import { ThemeToggler } from "gatsby-plugin-dark-mode"
 
 const Layout = ({ location, title, children }) => {
+  const [isDarkMode, setIsDarkMode] = React.useState(false)
   const rootPath = `${__PATH_PREFIX__}/`
   const isRootPath = location.pathname === rootPath
   let header
@@ -28,8 +29,11 @@ const Layout = ({ location, title, children }) => {
           <label>
             <input
               type="checkbox"
-              onChange={e => toggleTheme(e.target.checked ? "dark" : "light")}
-              checked={theme === "dark"}
+              onChange={e => {
+                setIsDarkMode(!isDarkMode)
+                toggleTheme(isDarkMode ? "light" : "dark")
+              }}
+              checked={isDarkMode}
             />{" "}
             Dark mode
           </label>
